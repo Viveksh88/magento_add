80a15a52b914029c678567f87a3416654a0a7c16
Viveksh88 <vivek_sharma@excellencetechnologies.in>
2018-12-28 11:43:22 +0530

    28th Dec Event

--- Crud/Setup/InstallSchema.php
+++ Crud/Setup/InstallSchema.php
@@ -70,6 +70,74 @@ class InstallSchema implements \Magento\Framework\Setup\InstallSchemaInterface
         );
         $installer->getConnection()->createTable($table);//$table end here
 
+        $tableB = $installer->getConnection()->newTable(
+            $installer->getTable('excellence_crud_orderSuccessDetails')
+        )->addColumn(
+            'excellence_crud_orderId',
+            \Magento\Framework\DB\Ddl\Table::TYPE_INTEGER,
+            null,
+            [ 'identity' => true, 'nullable' => false, 'primary' => true, 'unsigned' => true, ],
+            'Entity ID'
+        )->addColumn(
+            'grandTotal',
+            \Magento\Framework\DB\Ddl\Table::TYPE_TEXT,
+            255,
+            [ 'nullable' => false, ],
+            'totalAmount'
+        )->addColumn(
+            'customerId',
+            \Magento\Framework\DB\Ddl\Table::TYPE_INTEGER,
+            255,
+            [ 'nullable' => false, ],
+            'CustomerId'
+        )->addColumn(
+            'customerEmail',
+            \Magento\Framework\DB\Ddl\Table::TYPE_TEXT,
+            255,
+            [ 'nullable' => false, ],
+            'customerEmail'
+        )->addColumn(
+            'customerFirstname',
+            \Magento\Framework\DB\Ddl\Table::TYPE_TEXT,
+            255,
+            [ 'nullable' => false, ],
+            'customerfName'
+        )->addColumn(
+            'customerLastname',
+            \Magento\Framework\DB\Ddl\Table::TYPE_TEXT,
+            '2M',
+             [],
+            [ 'nullable' => false, ],
+            'customerLname'
+        )->addColumn(
+            'currencyCode',
+            \Magento\Framework\DB\Ddl\Table::TYPE_TEXT,
+            '2M',
+             [],
+            [ 'nullable' => false, ],
+            'currencyCode'
+        )->addColumn(
+            'creation_time',
+            \Magento\Framework\DB\Ddl\Table::TYPE_TIMESTAMP,
+            null,
+            [ 'nullable' => false, 'default' => \Magento\Framework\DB\Ddl\Table::TIMESTAMP_INIT, ],
+            'Creation Time'
+        )->addColumn(
+            'update_time',
+            \Magento\Framework\DB\Ddl\Table::TYPE_TIMESTAMP,
+            null,
+            [ 'nullable' => false, 'default' => \Magento\Framework\DB\Ddl\Table::TIMESTAMP_INIT_UPDATE, ],
+            'Modification Time'
+        )->addColumn(
+            'is_active',
+            \Magento\Framework\DB\Ddl\Table::TYPE_SMALLINT,
+            null,
+            [ 'nullable' => false, 'default' => '1', ],
+            'Is Active'
+        );
+        $installer->getConnection()->createTable($tableB);//$tableB end here
+
+
         $table_A = $installer->getConnection()->newTable(//$table_A starts
             $installer->getTable('excellence_crud_address')
         )->addColumn(
